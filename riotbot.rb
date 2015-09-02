59626417babb3c23513dc14e4ffe4c83083bd952
Cooper Quintin <cooperq@eff.org>
2015-09-02 14:56:57 -0700

    ftp capability

--- riotbot.rb
+++ riotbot.rb
@@ -3,13 +3,27 @@ require 'cinch'
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.freenode.org"
-    c.channels = ["#riotcon"]
+    c.channels = ["#riotbot"]
     c.nick = "riotbot"		  
   end
 
   on :message, "hello" do |m|
     m.reply "Hello, #{m.user.nick}"
   end
+
+  on :message, /cop|pig|police|popo|swine|fiveoh|5-0|5oh|onetime|one time/ do |m|
+    m.reply random_ftp
+  end
+  
+end
+
+def random_ftp()
+  return [ 
+    "Fuck the police!", 
+    "ACAB", 
+    "The only good cop is a dead cop", 
+    "Cops! Pigs! Murderers!"
+  ].shuffle.sample
 end
 
 bot.start
